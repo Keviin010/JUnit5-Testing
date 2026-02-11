1abf036d85138c2a554afbfd5321c54e4f8ffede
keviin <140296481+Keviin010@users.noreply.github.com>
2026-02-10 22:29:05 -0500

    Delete src directory

--- src/main/java/org/junit5app/exceptions/DineroInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package org.junit5app.exceptions;
-
-public class DineroInsuficienteException extends RuntimeException{
-    public DineroInsuficienteException(String message) {
-        super(message);
-    }
-}
